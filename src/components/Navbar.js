fdbd46c1730b27a77a19576ebc5220874cc7c0f2
gsmith77 <grantsmith1256@gmail.com>
2019-11-18 12:38:26 -0800

    added dropdown button, now time to customize it to use filters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import Filter from './Filter'
+import Dropdown from 'react-bootstrap/Dropdown'
+
 
 
  const Navbar = ()=>{
@@ -11,6 +13,17 @@ import Filter from './Filter'
                     <Link to="/" className="brand-logo">Shop Zara</Link>
                     
                     <ul className="right hide-on-med-and-down">
+                        <Dropdown style={{'float':'left'}}>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                Dropdown Button
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                         {/* <Filter /> */}
                         <li><Link to="/">Shop</Link></li>
                         <li><Link to="/cart">My cart</Link></li>
